14321521d2517c7562385f549efd7018967337ce
HumanKK9 <154393014+HumanKK9@users.noreply.github.com>
2025-09-15 15:03:34 +0700

    Add files via upload

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,5 @@
+zq = input()
+if zq == 0:
+    print("This number is equal to zero.")
+else:
+    print("This number is different from zero.")
---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,8 @@
+nqs = input()
+nq = int(nqs)
+if nq > 0:
+    print("This number is positive.")
+elif nq < 0:
+    print("This number is negative.")
+else:
+    print("This number is both positive and negative.")
